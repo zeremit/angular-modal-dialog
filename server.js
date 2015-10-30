03fc751b0ad7b0ba98a46ec62ffc1397eb151ac4
Khorevich Dmitry <dmitry.khorevich@altoros.com>
2015-10-30 16:12:05 +0300

    change server

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 //Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var serveStatic = require('serve-static');
 
 //Lets define a port we want to listen to
 const PORT=3000;
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+//start server
+var app = express();
+app.use(serveStatic('app', {'index': ['index.html', 'index.htm']}));
+app.listen(PORT);
