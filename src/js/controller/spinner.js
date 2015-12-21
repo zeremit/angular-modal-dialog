1543334d2058d9f03c0a89f06591b6f4c0c35fde
zeremit <zeremit@gmail.com>
2015-12-21 22:23:59 +0300

    add spinner

--- src/js/controller/spinner.js
+++ src/js/controller/spinner.js
@@ -1,3 +1,11 @@
 app.controller('SpinnerCtrl',['$scope', function($scope){
 
+    $scope.runSpinner = function(){
+        $scope.showSpinner = true;
+    };
+
+    $scope.stopSpinner = function(){
+        $scope.showSpinner = false;
+    }
+
 }]);
