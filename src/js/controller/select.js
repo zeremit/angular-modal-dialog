da1800ed7894bec0966b90a0d38727276bad985b
zeremit <zeremit@gmail.com>
2015-12-21 22:04:40 +0300

    add select

--- src/js/controller/select.js
+++ src/js/controller/select.js
@@ -1,4 +1,4 @@
-modalApp.controller('SelectCtrl',['staticLoader', '$scope', function(staticLoader, $scope){
+app.controller('SelectCtrl',['staticLoader', '$scope', function(staticLoader, $scope){
     $scope.types = [];
     staticLoader.getType().success(function (data) {
         $scope.types = data;
